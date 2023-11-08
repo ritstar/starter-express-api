8f0565ccf58ce9424df8d48612678d7f21ae2108
ritstar <riteshjha0270@gmail.com>
2023-11-08 17:55:29 +0530

    AWS changes

--- index.js
+++ index.js
@@ -2,23 +2,71 @@ const express = require('express');
 const app = express();
 const libre = require('libreoffice-convert');
 const path = require('path');
+const aws = require('aws-sdk');
 const multer = require('multer');
-const upload = multer({ dest: 'uploads/' });
+const multerS3 = require('multer-s3');
+const fs = require('fs');
+
+aws.config.update({
+    secretAccessKey: process.env.AWS_SECRET_ACCESS_KEY,
+    accessKeyId: process.env.AWS_ACCESS_KEY_ID,
+    region: process.env.AWS_REGION
+});
+
+const s3 = new aws.S3();
+
+const upload = multer({
+    storage: multerS3({
+        s3: s3,
+        bucket: process.env.AWS_BUCKET_NAME,
+        key: function (req, file, cb) {
+            cb(null, file.originalname); //use Date.now() for unique file keys
+        }
+    })
+});
 
 app.post('/convert', upload.single('file'), (req, res) => {
-  const inputFile = path.join(__dirname, req.file.path);
-  const outputFile = path.join(__dirname, 'output.pdf');
+    const inputFileKey = req.file.key;
+    const outputFileKey = `${path.parse(inputFileKey).name}.pdf`;
 
-  console.log(`Input file path: ${inputFile}`);
-  console.log(`Output file path: ${outputFile}`);
+    const params = {
+        Bucket: process.env.AWS_BUCKET_NAME,
+        Key: inputFileKey
+    };
 
-  libre.convert(inputFile, outputFile, (err) => {
-    if (err) {
-      return res.status(500).send('Error converting file');
-    }
+    s3.getObject(params, (err, data) => {
+        if (err) {
+            console.log(err);
+            return res.status(500).send('Error getting file from S3');
+        }
 
-    res.send('File converted successfully!');
-  });
+        const tempInputFile = path.join('/tmp', inputFileKey);
+        const tempOutputFile = path.join('/tmp', outputFileKey);
+
+        fs.writeFileSync(tempInputFile, data.Body);
+
+        libre.convert(tempInputFile, '.pdf', undefined, (err, done) => {
+            if (err) {
+                console.log(err);
+                return res.status(500).send('Error converting file');
+            }
+
+            fs.writeFileSync(tempOutputFile, done);
+
+            s3.upload({
+                Bucket: process.env.AWS_BUCKET_NAME,
+                Key: outputFileKey,
+                Body: fs.createReadStream(tempOutputFile)
+            }, (err, data) => {
+                if (err) {
+                    console.log(err);
+                    return res.status(500).send('Error uploading file to S3');
+                }
+
+                res.send({ downloadUrl: data.Location });
+            });
+        });
+    });
 });
 
 app.listen(process.env.PORT || 3000);
