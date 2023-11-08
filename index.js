83dc0d3bea7893d2f4d5db20e8f935fc4a1f757a
ritstar <riteshjha0270@gmail.com>
2023-11-08 18:29:53 +0530

    ENV setup

--- index.js
+++ index.js
@@ -2,22 +2,22 @@ const express = require('express');
 const app = express();
 const libre = require('libreoffice-convert');
 const path = require('path');
-const aws = require('aws-sdk');
 const multer = require('multer');
 const multerS3 = require('multer-s3');
+const S3FS = require('s3fs');
 const fs = require('fs');
+require('dotenv').config();
 
-aws.config.update({
-    secretAccessKey: process.env.AWS_SECRET_ACCESS_KEY,
+console.log("Buccket=",process.env.AWS_BUCKET_NAME);
+const s3fsImpl = new S3FS(process.env.AWS_BUCKET_NAME, {
     accessKeyId: process.env.AWS_ACCESS_KEY_ID,
+    secretAccessKey: process.env.AWS_SECRET_ACCESS_KEY,
     region: process.env.AWS_REGION
 });
 
-const s3 = new aws.S3();
-
 const upload = multer({
     storage: multerS3({
-        s3: s3,
+        s3: s3fsImpl,
         bucket: process.env.AWS_BUCKET_NAME,
         key: function (req, file, cb) {
             cb(null, file.originalname); //use Date.now() for unique file keys
@@ -29,12 +29,8 @@ app.post('/convert', upload.single('file'), (req, res) => {
     const inputFileKey = req.file.key;
     const outputFileKey = `${path.parse(inputFileKey).name}.pdf`;
 
-    const params = {
-        Bucket: process.env.AWS_BUCKET_NAME,
-        Key: inputFileKey
-    };
 
-    s3.getObject(params, (err, data) => {
+    s3fsImpl.readFile(inputFileKey, (err, data) => {
         if (err) {
             console.log(err);
             return res.status(500).send('Error getting file from S3');
@@ -43,7 +39,7 @@ app.post('/convert', upload.single('file'), (req, res) => {
         const tempInputFile = path.join('/tmp', inputFileKey);
         const tempOutputFile = path.join('/tmp', outputFileKey);
 
-        fs.writeFileSync(tempInputFile, data.Body);
+        fs.writeFileSync(tempInputFile, data);
 
         libre.convert(tempInputFile, '.pdf', undefined, (err, done) => {
             if (err) {
@@ -53,17 +49,14 @@ app.post('/convert', upload.single('file'), (req, res) => {
 
             fs.writeFileSync(tempOutputFile, done);
 
-            s3.upload({
-                Bucket: process.env.AWS_BUCKET_NAME,
-                Key: outputFileKey,
-                Body: fs.createReadStream(tempOutputFile)
-            }, (err, data) => {
+            s3fsImpl.writeFile(outputFileKey, done, (err) => {
                 if (err) {
                     console.log(err);
                     return res.status(500).send('Error uploading file to S3');
                 }
 
-                res.send({ downloadUrl: data.Location });
+                const downloadUrl = `https://${process.env.AWS_BUCKET_NAME}.s3.${process.env.AWS_REGION}.amazonaws.com/${outputFileKey}`;
+                res.send({ downloadUrl: downloadUrl });
             });
         });
     });
