b98411369f889e6b71895376d150a9aff34b7489
= <=>
2023-11-09 11:58:40 +0530

    AWS-SDK

--- index.js
+++ index.js
@@ -3,60 +3,73 @@ const app = express();
 const libre = require('libreoffice-convert');
 const path = require('path');
 const multer = require('multer');
-const multerS3 = require('multer-s3');
-const S3FS = require('s3fs');
+const AWS = require('aws-sdk');
 const fs = require('fs');
+const stream = require('stream');
 require('dotenv').config();
 
-console.log("Buccket=",process.env.AWS_BUCKET_NAME);
-const s3fsImpl = new S3FS(process.env.AWS_BUCKET_NAME, {
+console.log("Bucket=",process.env.CYCLIC_BUCKET_NAME);
+
+const s3 = new AWS.S3({
     accessKeyId: process.env.AWS_ACCESS_KEY_ID,
     secretAccessKey: process.env.AWS_SECRET_ACCESS_KEY,
     region: process.env.AWS_REGION
 });
 
-const upload = multer({
-    storage: multerS3({
-        s3: s3fsImpl,
-        bucket: process.env.AWS_BUCKET_NAME,
-        key: function (req, file, cb) {
-            cb(null, file.originalname); //use Date.now() for unique file keys
-        }
-    })
-});
+const upload = multer();
 
 app.post('/convert', upload.single('file'), (req, res) => {
-    const inputFileKey = req.file.key;
+    if (!req.file) {
+        return res.status(400).send('No file was uploaded.');
+    }
+
+    const inputFileKey = req.file.originalname;
     const outputFileKey = `${path.parse(inputFileKey).name}.pdf`;
 
+    const uploadParams = {
+        Bucket: process.env.CYCLIC_BUCKET_NAME,
+        Key: inputFileKey,
+        Body: req.file.buffer
+    };
 
-    s3fsImpl.readFile(inputFileKey, (err, data) => {
+    s3.upload(uploadParams, (err, data) => {
         if (err) {
             console.log(err);
-            return res.status(500).send('Error getting file from S3');
+            return res.status(500).send('Error uploading file to S3');
         }
 
-        const tempInputFile = path.join('/tmp', inputFileKey);
-        const tempOutputFile = path.join('/tmp', outputFileKey);
+        const downloadParams = {
+            Bucket: process.env.CYCLIC_BUCKET_NAME,
+            Key: inputFileKey
+        };
 
-        fs.writeFileSync(tempInputFile, data);
-
-        libre.convert(tempInputFile, '.pdf', undefined, (err, done) => {
+        s3.getObject(downloadParams, (err, data) => {
             if (err) {
                 console.log(err);
-                return res.status(500).send('Error converting file');
+                return res.status(500).send('Error downloading file from S3');
             }
 
-            fs.writeFileSync(tempOutputFile, done);
-
-            s3fsImpl.writeFile(outputFileKey, done, (err) => {
+            libre.convert(data.Body, '.pdf', undefined, (err, done) => {
                 if (err) {
                     console.log(err);
-                    return res.status(500).send('Error uploading file to S3');
+                    return res.status(500).send('Error converting file');
                 }
 
-                const downloadUrl = `https://${process.env.AWS_BUCKET_NAME}.s3.${process.env.AWS_REGION}.amazonaws.com/${outputFileKey}`;
-                res.send({ downloadUrl: downloadUrl });
+                const uploadConvertedParams = {
+                    Bucket: process.env.CYCLIC_BUCKET_NAME,
+                    Key: outputFileKey,
+                    Body: done
+                };
+
+                s3.upload(uploadConvertedParams, (err, data) => {
+                    if (err) {
+                        console.log(err);
+                        return res.status(500).send('Error uploading converted file to S3');
+                    }
+
+                    const downloadUrl = data.Location;
+                    res.send({ downloadUrl: downloadUrl });
+                });
             });
         });
     });
