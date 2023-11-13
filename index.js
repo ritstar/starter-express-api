e353cf537cdcfcde61f326ad632914e40a3f9288
= <=>
2023-11-13 13:58:50 +0530

    Logginf token

--- index.js
+++ index.js
@@ -23,9 +23,9 @@ const upload = multer();
 function authenticateToken(req, res, next) {
     const authHeader = req.headers['authorization'];
     const token = authHeader && authHeader.split(' ')[1];
-  
+    console.log(token);
     if (token == null) return res.sendStatus(401); // if there isn't any token
-  
+    
     jwt.verify(token, process.env.BEARER_TOKEN, (err, user) => {
       if (err) return res.sendStatus(403);
       req.user = user;
