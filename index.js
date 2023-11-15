108c73465d72bb0eafa845a3aab848a9844df9bf
= <=>
2023-11-15 19:14:25 +0530

    Removed Comment credential

--- index.js
+++ index.js
@@ -10,11 +10,6 @@ let cors = require("cors");
 app.use(cors());
 
 const s3 = new AWS.S3(
-//     {
-//     accessKeyId: process.env.AWS_ACCESS_KEY_ID,
-//     secretAccessKey: process.env.AWS_SECRET_ACCESS_KEY,
-//     region: process.env.AWS_REGION
-// }
 );
 
 const upload = multer();
