a434b2b1ac0b92233af31b0693ef2907f076d23d
= <=>
2023-11-21 17:52:01 +0530

    Jimp

--- routes/convert.js
+++ routes/convert.js
@@ -2,7 +2,7 @@ const express = require('express');
 const router = express.Router();
 const path = require('path');
 const multer = require('multer');
-const sharp = require('sharp');
+const Jimp = require('jimp');
 const AWS = require('aws-sdk');
 const fs = require('fs');
 require('dotenv').config();
@@ -22,37 +22,43 @@ router.post('/', upload.single('image'), async (req, res) => {
     }
 
     try {
-        const buffer = await sharp(req.file.buffer).toFormat(format).toBuffer();
-
-        const key = `${Date.now().toString()}.${format}`; // use a timestamp for unique file names
-
-        const uploadParams = {
-            Bucket: process.env.CYCLIC_BUCKET_NAME, // replace with your bucket name
-            Key: key,
-            Body: buffer,
-            ACL: 'public-read' // this will make the uploaded file publicly readable
-        };
-
-        await s3.upload(uploadParams).promise();
-
-        const urlParams = {
-            Bucket: process.env.CYCLIC_BUCKET_NAME, // replace with your bucket name
-            Key: key,
-            Expires: 300 // 5 minutes
-        };
-
-        const url = s3.getSignedUrl('getObject', urlParams);
-        res.send({ url: url });
-
-        // Schedule deletion of the image after 30 minutes
-        setTimeout(async () => {
-            try {
-                await s3.deleteObject({ Bucket: 'your-bucket-name', Key: key }).promise();
-                console.log(`Successfully deleted image ${key} from S3`);
-            } catch (err) {
-                console.error(`Error deleting image ${key} from S3:`, err);
+        const image = await Jimp.read(req.file.buffer);
+        image.getBuffer(Jimp[`MIME_${format.toUpperCase()}`], async (err, buffer) => {
+            if (err) {
+                console.log(err);
+                return res.status(500).send('Error converting image.');
             }
-        }, 30 * 60 * 1000); // 30 minutes
+
+            const key = `${Date.now().toString()}.${format}`; // use a timestamp for unique file names
+
+            const uploadParams = {
+                Bucket: process.env.CYCLIC_BUCKET_NAME, // replace with your bucket name
+                Key: key,
+                Body: buffer,
+                ACL: 'public-read' // this will make the uploaded file publicly readable
+            };
+
+            await s3.upload(uploadParams).promise();
+
+            const urlParams = {
+                Bucket: process.env.CYCLIC_BUCKET_NAME, // replace with your bucket name
+                Key: key,
+                Expires: 1800 // 30 minutes
+            };
+
+            const url = s3.getSignedUrl('getObject', urlParams);
+            res.send({ url: url });
+
+            // Schedule deletion of the image after 30 minutes
+            setTimeout(async () => {
+                try {
+                    await s3.deleteObject({ Bucket: 'your-bucket-name', Key: key }).promise();
+                    console.log(`Successfully deleted image ${key} from S3`);
+                } catch (err) {
+                    console.error(`Error deleting image ${key} from S3:`, err);
+                }
+            }, 30 * 60 * 1000); // 30 minutes
+        });
     } catch (err) {
         console.log(err);
         res.status(500).send('Error converting and uploading image.');
